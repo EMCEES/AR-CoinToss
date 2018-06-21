245e96007f7af267541735cca30a7ce4c5fde483
Justin Trautman <justin@modularmobile.net>
2018-06-21 16:04:32 -0600

    Removed test coin and added orange

--- ARCoinToss/ViewController.swift
+++ ARCoinToss/ViewController.swift
@@ -30,7 +30,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene(named: "art.scnassets/3d-model.dae")!
         
         // Set the scene to the view
         sceneView.scene = scene
